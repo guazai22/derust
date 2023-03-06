bb70bc573c1cca6aff63d29357f6fb4915f52473
张晋兵 <10212925+gzjb22@user.noreply.gitee.com>
2023-03-07 00:36:35 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     let a = String::from("123");
     // let b = a.clone();
     let b = a.clone();
-    // let c = a.clone();
     let c = a.clone();
+    // let c = a;
     println!("{}", b);
     println!("{}", c);
 }
